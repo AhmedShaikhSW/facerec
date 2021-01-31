5b2a53480d2e52e64e029ac09a2814f62637f549
Ahmed Shaikh <ahmed@reportmate.ca>
2021-01-31 23:42:19 +0530

    change input source to video mp4

--- face_rec.py
+++ face_rec.py
@@ -3,6 +3,8 @@
 ## in terminal
 
 import os
+import pickle
+import time
 import face_recognition
 import cv2
 
@@ -12,7 +14,7 @@ FRAME_THICKNESS = 3
 FONT_THICKNESS = 2
 MODEL = "cnn"   # can also use hog, better for cpu-only
 
-video = cv2.VideoCapture(0)
+video = cv2.VideoCapture("facerecvid.mp4")
 
 print("Loading known faces...")
 
@@ -22,10 +24,14 @@ known_names = []
 for name in os.listdir(KNOWN_FACES_DIR):
     for filename in os.listdir(f"{KNOWN_FACES_DIR}/{name}"):
     # can also use os.join for better os independency
-        image = face_recognition.load_image_file(f"{KNOWN_FACES_DIR}/{name}/{filename}")
-        encoded_image = face_recognition.face_encodings(image)[0]
+        encoded_image = pickle.load(open(f"{name}/{filename}", "rb"))
         known_faces.append(encoded_image)
-        known_names.append(name)
+        known_names.append(int(name))
+
+if len(known_names) > 0:
+    next_id = max(known_names) + 1
+else:
+    next_id = 0
 
 print("Checking unknown faces...")
 
@@ -42,21 +48,29 @@ while True:
         if True in results:
             match = known_names[results.index(True)]
             print(f"Match! {match}")
-            color = [0, 255, 0]
+        else:
+            match = str(next_id)
+            next_id += 1
+            known_names.append(match)
+            known_faces.append(face_encoding)
+            os.mkdir(f"{KNOWN_FACES_DIR}/{match}")
+            pickle.dump(face_encoding, open(f"{KNOWN_FACES_DIR}/{match}/{match}-{int(time.time())}.pkl", "wb"))
+        
+        color = [0, 255, 0]
 
-            top_left = (face_location[0], face_location[3])
-            bottom_right = (face_location[1], face_location[2])
-            cv2.rectangle(image, top_left, bottom_right, color, FRAME_THICKNESS)
+        top_left = (face_location[0], face_location[3])
+        bottom_right = (face_location[1], face_location[2])
+        cv2.rectangle(image, top_left, bottom_right, color, FRAME_THICKNESS)
 
-            top_left = (face_location[3], face_location[2])
-            bottom_right = (face_location[1], face_location[2]+22)
-            cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
-            cv2.putText(
-                image, match, (face_location[3]+10, face_location[2]+15),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), FONT_THICKNESS
-            )
+        top_left = (face_location[3], face_location[2])
+        bottom_right = (face_location[1], face_location[2]+22)
+        cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
+        cv2.putText(
+            image, match, (face_location[3]+10, face_location[2]+15),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), FONT_THICKNESS
+        )
 
-    cv2.imshow(filename, image)
+    cv2.imshow("", image)
     
     if cv2.waitKey(1) & 0xFF == ord("q"):
         # break if user presses the q key
